714a7a74f2d685c491ddb9b53965235b303876ed
Allen Rohner <arohner@gmail.com>
2012-03-19 01:48:13 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject bond "0.1.0"
+  :description "Spying library for testing"
+  :dev-dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ src/bond/core.clj
@@ -0,0 +1,34 @@
+(ns bond.core)
+
+(defn with-spies)
+
+(defn spy
+  "wrap f, keeping track of its call count and arguments"
+  [f]
+  (let [calls (atom [])
+        old-f f]
+    (with-meta (fn [& args]
+                 (try
+                   (let [resp (apply old-f args)]
+                     (swap! calls conj {:args args
+                                        :return resp})
+                     resp)
+                   (catch Exception e
+                     (swap! calls conj {:args args
+                                        :throw e})
+                     (throw e))))
+      {::calls calls})))
+
+(defn calls [f]
+  (-> f (meta) ::calls (deref)))
+
+(defmacro with-spy
+  "Takes a var pointing at a fn. Rebinds the fn to track call counts"
+  [v & body]
+  `(binding [~v (spy ~v)]
+     (do ~@body)))
+
+(defmacro with-spy-redef
+  [v & body]
+  `(with-redefs [~v (spy ~v)]
+     (do ~@body)))
