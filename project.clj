3d5ba266e6fa22130e6603ec8e5b43a6825ac612
Allen Rohner <arohner@gmail.com>
2012-03-30 22:05:41 -0700

    Adds with-stub. Improve documentation. Drop with-spy-redef because it's unnecessary

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject bond "0.2.1"
+(defproject bond "0.2.2"
   :description "Spying library for testing"
   :dev-dependencies [[org.clojure/clojure "1.3.0"]])
--- src/bond/james.clj
+++ src/bond/james.clj
@@ -19,16 +19,25 @@
                      (throw e))))
       {::calls calls})))
 
-(defn calls [f]
+(defn calls
+  "Takes one arg, a fn that has previously been spied. Returns a seq
+  of maps, one per call. Each map contains the keys :args and :return
+  or :throw"
+  [f]
   (-> f (meta) ::calls (deref)))
 
 (defmacro with-spy
-  "Takes a var pointing at a fn. Rebinds the fn to track call counts"
+  "Takes a var pointing at a fn. Modifies the fn to track call counts, but does not change the fn's behavior"
   [v & body]
   `(with-redefs [~v (spy ~v)]
      (do ~@body)))
 
-(defmacro with-spy-redef
-  [v & body]
-  `(with-redefs [~v (spy ~v)]
-     (do ~@body)))
+(defmacro with-stub
+  "Takes a vector of fn vars. Replaces each fn with one that takes any
+  number of args and returns nil. Also spies the stubbed-fn"
+  [vs & body]
+
+  `(with-redefs ~(->> (mapcat (fn [v]
+                                [v `(spy (constantly nil))]) vs)
+                      (vec))
+     ~@body))
