2e481695fa533e39af317497b35eb0f716f6e94c
Allen Rohner <arohner@gmail.com>
2013-03-20 16:59:56 -0700

    Bump the version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject bond "0.2.3"
+(defproject bond "0.2.4"
   :description "Spying library for testing"
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]])
+  :dev-dependencies [[org.clojure/clojure "1.5.1"]])
