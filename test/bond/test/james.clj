3b94965c79f5acb0a40ae200e8e7096c2fc455dc
Glen Mailer <glenjamin@gmail.com>
2013-03-23 12:47:12 +0000

    Allow use of map in with-stub to specify return values

--- test/bond/test/james.clj
+++ test/bond/test/james.clj
@@ -7,14 +7,28 @@
 
 (defn bar [x] (println "bar!") (* 2 x))
 
-(deftest spy-works []
+(defn baz [x] (throw (Exception. (str x))))
+
+(deftest spy-works
   (bond/with-spy [foo]
     (foo 1)
     (foo 2)
-    (is (= 2 (-> foo bond/calls count)))))
+    (is (= 2 (-> foo bond/calls count)))
+    (is (= {:args [1] :return 2} (-> foo bond/calls first)))
+    (is (= {:args [2] :return 4} (-> foo bond/calls second)))))
 
-(deftest stub-works []
+(deftest spy-catches
+  (bond/with-spy [baz]
+    (is (thrown? Exception (baz :a)))
+    (is (= Exception (type (-> baz bond/calls first :throw))))))
+
+(deftest stub-works
   (is (= ""
          (with-out-str
            (bond/with-stub [bar]
              (bar 3))))))
+
+(deftest stub-maps
+  (bond/with-stub {foo 1}
+    (is (= 1 (foo 3)))
+    (is (= {:args [3] :return 1} (-> foo bond/calls first)))))
