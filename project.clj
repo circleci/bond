b9924a098a977535d331bebe11e03aa369a5ef2f
Glen Mailer <glenjamin@gmail.com>
2013-05-19 15:55:30 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject q "0.1.0-SNAPSHOT"
+(defproject q "0.1.0"
   :description "Spy gadgets for Clojure testing"
+  :url "http://github.com/glenjamin/q"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
