3963c3526c284c1e7663464abda327bcb706056b
Allen Rohner <arohner@gmail.com>
2013-09-06 16:51:43 -0500

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bond "0.2.5"
+(defproject bond "0.2.6"
   :description "Spying library for testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
