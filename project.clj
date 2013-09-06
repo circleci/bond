3298846e3ff54dd911b1af97223327923c557675
Allen Rohner <arohner@gmail.com>
2013-09-06 16:51:28 -0500

    Move clojure into deps, not :dev-deps.

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Spying library for testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dev-dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
