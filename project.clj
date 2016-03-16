b797d563169d8e606553fba8178787d967f9474f
Justin Cowperthwaite <justinc474@gmail.com>
2016-03-16 16:57:23 -0700

    bumped the version to 0.2.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circleci/bond "0.2.7"
+(defproject circleci/bond "0.2.8"
   :description "Spying library for testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
