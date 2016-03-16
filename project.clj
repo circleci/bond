77cf2747b4bd1303d7bc4d84ffff5629aa760e4c
Justin Cowperthwaite <justinc474@gmail.com>
2016-03-16 16:13:50 -0700

    removed cloverage since it doesn't support cljc

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]
-            [lein-test-out "0.3.1" :exclusions [org.clojure/clojure]]])
+  :plugins [[lein-test-out "0.3.1" :exclusions [org.clojure/clojure]]])
