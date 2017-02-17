913d61c83c8e062515556380fd5a7f0059dc2a30
Justin Cowperthwaite <justinc474@gmail.com>
2017-02-16 17:04:06 -0800

    added auto-deployment to clojars at version 0.2.build-num

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject circleci/bond "0.2.9"
+  :version-spec "0.2.~{:env/circle_build_num}"
   :description "Spying library for testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
