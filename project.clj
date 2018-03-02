29fd053b482e494effffce8a0da8b0a4221cdd54
Anton Panferov <smaant@gmail.com>
2018-03-02 11:20:52 -0800

    bumped version to 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circleci/bond "0.3.0"
+(defproject circleci/bond "0.3.1"
   :description "Spying library for testing"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
