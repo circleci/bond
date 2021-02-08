2fe9055dd1eabe67f85c8e097a2c78874a598b84
Dan Carley <dan.carley@gmail.com>
2021-02-08 09:43:10 +0000

    Upgrade Clojure and Cloverage versions
    So that we test against a more recent and representative version of
    Clojure and no longer need to pin an older version of Cloverage for
    compatibility with it.

--- project.clj
+++ project.clj
@@ -3,10 +3,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies []
-  :plugins [[lein-cloverage "1.0.9"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  ;; by default, lein-cloverage will
-                                  ;; use the latest release of
-                                  ;; cloverage. Specify the version of
-                                  ;; cloverage to override this.
-                                  [cloverage "1.0.9"]]}})
+  :plugins [[lein-cloverage "1.2.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.2"]]}})
